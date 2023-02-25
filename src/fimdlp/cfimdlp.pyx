900cccf76b47f2ad7be5b1cc12e2ad70aacf26bf
Ricardo Montañana <rmontanana@gmail.com>
2023-02-25 18:52:21 +0100

    Update discretizer to new library

--- src/fimdlp/cfimdlp.pyx
+++ src/fimdlp/cfimdlp.pyx
@@ -3,18 +3,22 @@
 from libcpp.vector cimport vector
 from libcpp.string cimport string
 
+cdef extern from "limits.h":
+    cdef int INT_MAX
 cdef extern from "../cppmdlp/CPPFImdlp.h" namespace "mdlp":
     ctypedef float precision_t
     cdef cppclass CPPFImdlp:
         CPPFImdlp() except + 
+        CPPFImdlp(int, int) except + 
         CPPFImdlp& fit(vector[precision_t]&, vector[int]&)
+        int get_depth()
         vector[precision_t] getCutPoints()
         string version()
         
 cdef class CFImdlp:
     cdef CPPFImdlp *thisptr
-    def __cinit__(self):
-        self.thisptr = new CPPFImdlp()
+    def __cinit__(self, int min_length=3, int max_depth=INT_MAX):
+        self.thisptr = new CPPFImdlp(min_length, max_depth)
     def __dealloc__(self):
         del self.thisptr
     def fit(self, X, y):
@@ -24,6 +28,8 @@ cdef class CFImdlp:
         return self.thisptr.getCutPoints()
     def get_version(self):
         return self.thisptr.version()
+    def get_depth(self):
+        return self.thisptr.get_depth()
     def __reduce__(self):
         return (CFImdlp, ())
 
